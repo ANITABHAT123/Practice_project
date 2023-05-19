48b5e6d745c831777c91dd5e0c3638381e735db9
anita <anitaramakrishna@gmail.com>
2023-05-19 12:09:03 +0530

    This is first program to staged

---
+++ gdb_example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int calculateSum(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int result = calculateSum(x, y);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
